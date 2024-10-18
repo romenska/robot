4a365be0a5efa933b5c763cac1cb6c6c6d0c6fd0
Olha <67200061+romenska@users.noreply.github.com>
2024-10-18 21:52:38 +0300

    две ноги

--- main.ts
+++ main.ts
@@ -1,4 +1,6 @@
+basic.showIcon(IconNames.Happy)
 let Direct_foot_right = 1
+let Direct_foot_left = -1
 let Foot_left = 90
 let Foot_right = 90
 let Leg_left = 0
@@ -9,8 +11,13 @@ pins.servoWritePin(AnalogPin.P2, Foot_left)
 pins.servoWritePin(AnalogPin.P3, Leg_left)
 basic.forever(function () {
     Foot_right += Direct_foot_right
+    Foot_left += Direct_foot_left
     pins.servoWritePin(AnalogPin.P0, Foot_right)
+    pins.servoWritePin(AnalogPin.P2, Foot_left)
     if (Foot_right >= 180 || Foot_right <= 0) {
         Direct_foot_right = Direct_foot_right * -1
+    }
+    if (Foot_left >= 180 || Foot_left <= 0) {
+        Direct_foot_left = Direct_foot_left * -1
     }
 })
